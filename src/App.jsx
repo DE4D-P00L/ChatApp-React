521f8bfe74084b581832cdc47418b301e5fc7c40
Prashant <jatinverma200025@gmail.com>
2024-02-05 11:22:07 +0530

    axios interceptors added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import { Toaster } from "react-hot-toast";
 import { useSelector } from "react-redux";
+import axios from "axios";
+
+axios.interceptors.request.use((request) => {
+  request.headers.testValue = "aaaaa";
+  return request;
+});
 
 function App() {
   const user = useSelector((state) => state.auth.user);
